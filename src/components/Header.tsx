0fd3531a4a3f5c56560fbcf5ecf2537450dba0c9
sdharamk14 <sdharamkar14@gmail.com>
2021-12-31 09:53:41 +0000

    fixing on click logo navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <nav className="flex justify-between items-center min-w-full h-14 bg-purple-400 px-5 py-2 mb-8">
       <div className="">
-        <a href="/">
+        <NavLink to="/">
           <div className="flex justify-center items-center text-white uppercase">
             <ChartPieIcon className="w-8 h-8" />
             &nbsp; Opportunist
           </div>
-        </a>
+        </NavLink>
       </div>
       <div className="space-x-3">
         <NavLink to="/login" className="text-white">
